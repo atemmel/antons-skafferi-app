255788fb0982355134b476adecfb5690c21e9033
atemmel <temmeladam@gmail.com>
2019-11-12 11:13:52 +0100

    First commit

---
+++ app/src/main/java/se/grupp1/antonsskafferi/MainActivity.java
@@ -0,0 +1,1 @@
+package se.grupp1.antonsskafferi;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);    }}
