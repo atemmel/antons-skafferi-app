25899e0a1eacc166f12fd5694120c844add152a4
Oscar Fredriksson <osfr1701@student.miun.se>
2019-11-19 15:39:16 +0100

    Created activities for booking and schedule

---
+++ app/src/main/java/se/grupp1/antonsskafferi/BookingActivity.java
@@ -0,0 +1,14 @@
+package se.grupp1.antonsskafferi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+    }
+}
