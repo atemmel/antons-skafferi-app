c4a5b7fa081ef69a15741ab774c3db94093467ec
Oscar Fredriksson <osfr1701@student.miun.se>
2019-11-20 20:48:14 +0100

    Created basic settings activity

---
+++ app/src/main/java/se/grupp1/antonsskafferi/SettingsActivity.java
@@ -0,0 +1,41 @@
+package se.grupp1.antonsskafferi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment()).commit();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat
+    {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
+        {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(myIntent);
+        return true;
+    }
+}
