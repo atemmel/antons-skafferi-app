35e8d2e5f562116ae678fa9a7f42b4769c6e3ce5
Oscar Fredriksson <osfr1701@student.miun.se>
2019-11-21 14:38:56 +0100

    Basic structure for navigating to edit lunch activity

---
+++ app/src/main/java/se/grupp1/antonsskafferi/EditLunchActivity.java
@@ -0,0 +1,45 @@
+package se.grupp1.antonsskafferi;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+public class EditLunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_lunch);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Lägga till ny lunchmeny", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Kopiera dessa två rader för att lägga en tillbaka-pil uppe till vänster
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent myIntent = new Intent(getApplicationContext(), AdminActivity.class);
+        startActivity(myIntent);
+        return true;
+    }
+}
