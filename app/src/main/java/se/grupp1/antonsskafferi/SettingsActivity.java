22303c9f5a900c5e5adb2786c3324bd1e05bb414
Oscar Fredriksson <osfr1701@student.miun.se>
2019-11-23 11:56:59 +0100

    renamed settings XML-file to have similar structure with the other files

--- app/src/main/java/se/grupp1/antonsskafferi/SettingsActivity.java
+++ app/src/main/java/se/grupp1/antonsskafferi/SettingsActivity.java
@@ -14,7 +14,7 @@ public class SettingsActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment()).commit();
