93de1bc2b257e451557e62c2a70b44083d5490bc
amandaericson <amer1501@student.miun.se>
2019-11-26 13:47:06 +0100

    New take order fragment

---
+++ app/src/main/java/se/grupp1/antonsskafferi/TakeOrderFragment.java
@@ -0,0 +1,23 @@
+package se.grupp1.antonsskafferi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class TakeOrderFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View root = inflater.inflate(R.layout.fragment_takeorder, container, false);
+
+        return root;
+
+    }
+
+}
