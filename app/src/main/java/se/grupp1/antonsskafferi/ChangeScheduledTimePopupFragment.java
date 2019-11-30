ef6c7dd8fcddb65a5f6957bd8ebd62403ff4e9dc
amandaericson <amer1501@student.miun.se>
2019-12-01 00:26:14 +0100

    Change of scheduled time button and startup for pop up window added

---
+++ app/src/main/java/se/grupp1/antonsskafferi/ChangeScheduledTimePopupFragment.java
@@ -0,0 +1,40 @@
+package se.grupp1.antonsskafferi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+
+public class ChangeScheduledTimePopupFragment extends DialogFragment {
+
+    public ChangeScheduledTimePopupFragment() {}
+
+
+    public static ChangeScheduledTimePopupFragment newInstance()
+    {
+        ChangeScheduledTimePopupFragment fragment = new ChangeScheduledTimePopupFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_change_cheduled_time_popup, container, false);
+
+        getDialog().getWindow().setBackgroundDrawableResource(R.drawable.dialog_rounded_bg);
+
+        v.findViewById(R.id.closeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+
+        return v;
+    }
+}
