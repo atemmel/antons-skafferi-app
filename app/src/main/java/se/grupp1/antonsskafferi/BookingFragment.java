b1bc9d6a385d5a89fed7127601930edb45e27327
manjaro97 <joca1604@student.miun.se>
2019-12-02 00:29:26 +0100

    Added DatePicker to Booking

--- app/src/main/java/se/grupp1/antonsskafferi/BookingFragment.java
+++ app/src/main/java/se/grupp1/antonsskafferi/BookingFragment.java
@@ -1,34 +1,65 @@
 package se.grupp1.antonsskafferi;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 public class BookingFragment extends Fragment {
+
+    TextView mTv;
+    Button mBtn;
+    DatePickerDialog dpd;
+    Calendar c;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View root = inflater.inflate(R.layout.fragment_booking, container, false);
 
 
+        mTv = root.findViewById(R.id.BookingDate);
+        mBtn = root.findViewById(R.id.Datepicker);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year =c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDay) {
+                        mTv.setText(mYear + "-" + (mMonth+1) + "-" + mDay);
+                    }
+                }, year, month, day);
+                dpd.show();
+            }
+        });
 
         Button SendBtn =  root.findViewById(R.id.SendBtn);
         SendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                EditText BookingFirstName = getView().findViewById(R.id.BookingFirstName);
-                EditText BookingLastName = getView().findViewById(R.id.BookingLastName);
-                EditText BookingPeopleAmount =  getView().findViewById(R.id.BookingPeopleAmount);
-                EditText BookingPhoneNr =  getView().findViewById(R.id.BookingPhoneNr);
-                EditText BookingTime =  getView().findViewById(R.id.BookingTime);
-                EditText BookingDate =  getView().findViewById(R.id.BookingDate);
+                EditText BookingFirstName = v.findViewById(R.id.BookingFirstName);
+                EditText BookingLastName = v.findViewById(R.id.BookingLastName);
+                EditText BookingPeopleAmount =  v.findViewById(R.id.BookingPeopleAmount);
+                EditText BookingPhoneNr =  v.findViewById(R.id.BookingPhoneNr);
+                EditText BookingTime =  v.findViewById(R.id.BookingTime);
+                EditText BookingDate =  v.findViewById(R.id.BookingDate);
 
                 String firstName = BookingFirstName.getText().toString();
                 String lastName = BookingLastName.getText().toString();
