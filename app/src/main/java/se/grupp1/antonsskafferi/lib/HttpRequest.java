3a4a647f307298651d32974bf9ba3e19c953f2fc
Oscar Fredriksson <osfr1701@student.miun.se>
2019-12-08 16:38:11 +0100

    Added documentation

--- app/src/main/java/se/grupp1/antonsskafferi/lib/HttpRequest.java
+++ app/src/main/java/se/grupp1/antonsskafferi/lib/HttpRequest.java
@@ -12,40 +12,72 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * En klass för att skicka en HttpRequest till en API. Klassen använder sig av AsyncTask för att
+ * requesten ska ske på en egen tråd i bakgrunden.
+ */
 public class HttpRequest extends AsyncTask<String, Integer, String>
 {
+    /**
+     * Ett interface för att skicka svaret från HttpRequesten till mainthread.
+     * En instans som implementerar interfacets funktion skickas in i klassens konstruktor.
+     */
     public interface Response
     {
+        /**
+         * Denna funktion körs när API-requesten har skickats och ett svar har mottagits och behandlats.
+         *
+         * @param output En sträng med det svar som mottogs från APIn.
+         * @param status Statuskoden för requesten
+         */
         void processFinish(String output, int status);
     }
 
-
     private Response delegate;
-
     private String requestMethod;
     private String payload;
     private boolean isPost;
     private int status;
 
+
+    /**
+     * @param delegate En implementerad instans av interfacet Response
+     */
     public HttpRequest(Response delegate)
     {
         this.delegate = delegate;
     }
 
-    public void setRequestMethod(String requestMethod) {
-        this.requestMethod = requestMethod;
+    /**
+     * Sätt vilken request metod som ska användas, kan vara t.ex GET, POST, DELETE, mfl.
+     *
+     * @param requestMethod Reuquestmetoden som ska användas.
+     */
+    public void setRequestMethod(String requestMethod)
+    {
+        this.requestMethod = requestMethod.toUpperCase();
+
         isPost = requestMethod.toUpperCase().equals("POST");
     }
 
-    public void setPayload(String payload) {
+    /**
+     * Sätt data som ska skickas till APIn, används framförallt vid en POST-request
+     *
+     * @param payload Den data som ska skickas.
+     */
+    public void setPayload(String payload)
+    {
         this.payload = payload;
     }
 
+    /**
+     * Denna metod körs när <code>httpRequest.execute(...);</code> körs och sker sedan i
+     * bakgrunden på en egen tråd.
+     *
+     * @param urls URLn till APIn.
+     * @return En sträng med resultatet från requesten.
+     */
     @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-    }
-
     protected String doInBackground(String... urls) {
         StringBuilder content = new StringBuilder();
         String line;
@@ -57,6 +89,7 @@ public class HttpRequest extends AsyncTask<String, Integer, String>
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
             connection.setRequestMethod(requestMethod);
+
             if(isPost) {
                 System.out.println("POSTING DATA");
                 connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
@@ -124,6 +157,12 @@ public class HttpRequest extends AsyncTask<String, Integer, String>
 
     }
 
+    /**
+     * När <code> doInBackground(...) </code> är klar och har fått svar från APIn, körs denna metod.
+     * Kör då <code> processFinish(...)</code> i responsinterfacet.
+     *
+     * @param result resultatet från <code> doInBackground(...) </code>
+     */
     @Override
     protected void onPostExecute(String result)
     {
