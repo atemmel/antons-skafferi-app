f567907be1482d89361f3ee697f6bf5dd2e13b73
Oscar Fredriksson <osfr1701@student.miun.se>
2019-12-12 11:06:53 +0100

    Edit schedule page form almost done

--- app/src/main/java/se/grupp1/antonsskafferi/fragments/EditScheduleFragment.java
+++ app/src/main/java/se/grupp1/antonsskafferi/fragments/EditScheduleFragment.java
@@ -1,12 +1,17 @@
 package se.grupp1.antonsskafferi.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,12 +21,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 import se.grupp1.antonsskafferi.R;
 import se.grupp1.antonsskafferi.lib.DatabaseURL;
 import se.grupp1.antonsskafferi.lib.HttpRequest;
+import se.grupp1.antonsskafferi.lib.StringFormatter;
 
 public class EditScheduleFragment extends Fragment {
 
@@ -39,6 +46,70 @@ public class EditScheduleFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         loadUsersToDropdown();
         addListenerOnSpinnerEmployeeSelection();
+
+        view.findViewById(R.id.datePickerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        TextView dateText = getView().findViewById(R.id.dateLabel);
+
+                        dateText.setText(StringFormatter.formatDate(year + "-" + (month + 1) + "-" + day));
+                    }
+                }, year, month, day);
+
+                datepickerdialog.show();
+            }
+        });
+
+        view.findViewById(R.id.startTimePickerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar currentTime = Calendar.getInstance();
+
+                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = currentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        TextView timeText = getView().findViewById(R.id.startTimeLabel);
+
+                        timeText.setText(StringFormatter.formatTime(hour + ":" + minute));
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
+            }
+        });
+
+        view.findViewById(R.id.endTimePickerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar currentTime = Calendar.getInstance();
+
+                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = currentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        TextView timeText = getView().findViewById(R.id.endTimeLabel);
+
+                        timeText.setText(StringFormatter.formatTime(hour + ":" + minute));
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
+            }
+        });
 
     }
 
@@ -122,7 +193,6 @@ public class EditScheduleFragment extends Fragment {
                     //TODO: Only one employee can have a certain name?
                     String employeeidToUser = usersToEmployees.get(usr);
 
-                    loadDatesToDropdown(employeeidToUser);
 
                 } catch (Exception e)
                 {
@@ -134,47 +204,4 @@ public class EditScheduleFragment extends Fragment {
         httpRequest.setRequestMethod("GET");
         httpRequest.execute(DatabaseURL.getEmployees);
     }
-
-
-    private void loadDatesToDropdown(String employeeidToUser)
-    {
-        final String employeeId = employeeidToUser;
-        final Spinner dropdown = getView().findViewById(R.id.dateDropdown);
-
-        HttpRequest httpRequest = new HttpRequest(new HttpRequest.Response() {
-            @Override
-            public void processFinish(String output, int status)
-            {
-                try
-                {
-                    JSONArray jsonArr = new JSONArray(output);
-
-                    Map<String, String> usersToEmployees = new HashMap<>();
-
-                    for(int i = 0; i < jsonArr.length(); i++)
-                    {
-                        JSONObject c = jsonArr.getJSONObject(i);
-
-                        String employee = c.getString("employeeid");
-                        String username = c.getString("username");
-
-                        usersToEmployees.put(username, employee);
-                    }
-
-                    //String id = usersToEmployees.get(usr);
-
-                    //ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, users);
-                    //dropdown.setAdapter(adapter);
-
-                } catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        httpRequest.setRequestMethod("GET");
-        httpRequest.execute(DatabaseURL.getEmployees);
-    }
-
 }
