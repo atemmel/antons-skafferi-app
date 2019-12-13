a7863d4693a91d0056788fc63a1dddad4d93db06
Oscar Fredriksson <osfr1701@student.miun.se>
2019-12-13 08:17:30 +0100

    Lunch layout

--- app/src/main/java/se/grupp1/antonsskafferi/fragments/EditLunchFragment.java
+++ app/src/main/java/se/grupp1/antonsskafferi/fragments/EditLunchFragment.java
@@ -1,22 +1,18 @@
 package se.grupp1.antonsskafferi.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import se.grupp1.antonsskafferi.R;
 
@@ -28,15 +24,19 @@ public class EditLunchFragment extends Fragment
     {
         final View root = inflater.inflate(R.layout.fragment_edit_lunch, container, false);
 
+        Spinner spinner = root.findViewById(R.id.alternativeDropdown);
 
-        /*FloatingActionButton fab = root.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Lägga till ny lunchmeny", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        ArrayList<String> alternativeArray = new ArrayList<>();
+
+        alternativeArray.add("Alternativ 1");
+        alternativeArray.add("Alternativ 2");
+        alternativeArray.add("Alternativ 3");
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, alternativeArray);
+
+        spinner.setAdapter(adapter);
+
 
         root.findViewById(R.id.addLunchButton).setOnClickListener(new View.OnClickListener()
         {
@@ -46,7 +46,7 @@ public class EditLunchFragment extends Fragment
                 EditText titleEditText = root.findViewById(R.id.titleLunchInputText);
                 String title = titleEditText.getText().toString();
 
-                EditText descriptionEditText = root.findViewById(R.id.descriptionLunchInputText);
+                EditText descriptionEditText = root.findViewById(R.id.typeLunchInputText);
                 String description = descriptionEditText.getText().toString();
 
                 //EditText dateEditText = root.findViewById(R.id.dateLunchInputText);
